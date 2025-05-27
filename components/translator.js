8a7556a8768daf196560e16445584a8639925a25
Joshua Wright <1981283+SuperJoshua@users.noreply.github.com>
2025-05-27 17:57:44 +0000

    unit and funct tests pass

--- components/translator.js
+++ components/translator.js
@@ -16,7 +16,7 @@ module.exports = Translator
 // Why am I expected to use a class instead of a function?
 //    I'm using a function.
 // Why four dictionaries instead of two? Why aren't they mirrored -- because they're not one-to-one?
-//    I'm using custom dictionaries; I didn't mirrored them.
+//    I'm using custom dictionaries; I didn't mirror them.
 
 const a2bdict = require("./a2bdict.js")
 const b2adict = require("./b2adict.js")
@@ -29,19 +29,19 @@ function translate(text, locale) {
    switch (locale) {
       case "american-to-british": {
          dict = a2bdict
-         // replace time and title
+         // Replace time and title. This is a hack.
          translation = text.replaceAll(/\b(\d{1,2}):(\d{1,2})\b/g, `<span class="highlight">$1.$2</span>`)
          translation = translation.replaceAll(/\b[DMP][a-z]+\./g, replacer)
          break
       }
       case "british-to-american": {
          dict = b2adict
-         // replace time and title
+         // Replace time and title. This is also a hack.
          translation = text.replaceAll(/\b(\d{1,2}).(\d{1,2})\b/g, `<span class="highlight">$1:$2</span>`)
          break
       }
       default: {
-         // this shouldn't be reachable
+         // This shouldn't be reachable.
          throw new Error("invalid locale request somehow made it into translate")
       }
    }
@@ -49,47 +49,64 @@ function translate(text, locale) {
    // This only worked for one word matches. I couldn't figure out how to get regular expressions to do what I wanted. So I went with something that I understood better -- arrays.
    // translation = translation.replaceAll(/\b\w+\b/gi, replacer)
    
-   // Yes, this is a principle example of something that could be refactored. But I'm not enjoying this task, and I'm not getting paid for it.
+   // This is a hack, and certainly should be refactored. But I'm not enjoying this task, and I'm not getting paid for it.
    const raw_arr = translation.split(' ')
    const new_arr = []
    for (let i = 0; i < raw_arr.length; i += 1) {
-      let el = raw_arr[i]
-      let punct = [',', '.'].includes(el.at(-1)) ? el.at(-1) : ''
-      el = punct ? el.slice(0, -1) : el
+      let one_el = raw_arr[i]
+      let two_el = i < raw_arr.length - 1 ? `${one_el} ${raw_arr[i + 1]}` : ''
+      let three_el = i < raw_arr.length - 2 ? `${two_el} ${raw_arr[i + 2]}` : ''
 
-      if (dict[el.toLowerCase()]) {
-         new_arr.push(`<span class="highlight">${dict[el.toLowerCase()]}</span>${punct}`)
+      let skip = false
+
+      if (three_el) {
+         do_three()
       }
-      else if (!punct && i < raw_arr.length - 1) {
-         let two_el = `${el} ${raw_arr[i + 1]}`
-         punct = [',', '.'].includes(two_el.at(-1)) ? two_el.at(-1) : ''
-         two_el = punct ? two_el.slice(0, -1) : two_el
+      else if (two_el && !skip) {
+         do_two()
+      }
+      else if (!skip) {
+         do_one()
+      }
+      else {throw new Error("Something is wrong with the logic.")}
 
-         if (dict[two_el.toLowerCase()]) {
-            new_arr.push(`<span class="highlight">${dict[two_el.toLowerCase()]}</span>${punct}`)
-            i += 1
-         }
-         else if (!punct && i < raw_arr.length - 2) {
-            let three_el = `${two_el} ${raw_arr[i + 2]}`
-            punct = [',', '.'].includes(three_el.at(-1)) ? three_el.at(-1) : ''
-            three_el = punct ? three_el.slice(0, -1) : three_el
-
-            if (dict[three_el.toLowerCase()]) {
-               new_arr.push(`<span class="highlight">${dict[three_el.toLowerCase()]}</span>${punct}`)
+      function do_three() {
+         const re_three = /\b\w+ \w+ \w+\b/
+         if ((three_el.match(re_three)) !== null) {
+            const result = three_el.replace(re_three, replacer)
+            if (result !== three_el) {
+               new_arr.push(result)
                i += 2
+               skip = true
             }
-            else {
-               new_arr.push(el)
-            }
+            else {do_two()}
          }
-         else {
-            new_arr.push(el)
-         }
+         else {do_two()}
       }
-      else {
-         new_arr.push(`${el}${punct}`)
+
+      function do_two() {
+         const re_two = /\b\w+ \w+\b/
+         if ((two_el.match(re_two)) !== null) {
+            const result = two_el.replace(re_two, replacer)
+            if (result !== two_el) {
+               new_arr.push(result)
+               i += 1
+               skip = true
+            }
+            else {do_one()}
+         }
+         else {do_one()}
+      }
+
+      function do_one(){
+         const re_one = /\b\w+\b/
+         if ((one_el.match(re_one)) !== null) {
+            new_arr.push(one_el.replace(re_one, replacer))
+         }
+         else {new_arr.push(one_el)}
       }
    }
+
    translation = new_arr.join(' ')
 
    if (text === translation) {translation = "Everything looks good to me!"}
